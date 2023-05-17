9a6edb3db87aae031e1cb9c63d3b5cb2ab6f6016
Giulia Ivan <eaagril@students.eaaa.dk>
2023-05-17 13:52:58 +0200

    comments

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ let draggedItem = null;
 for (let i = 0; i < list_items.length; i++) {
     const item = list_items[i];
 
+    // This code is used to implement drag and drop functionality.
+    // It sets up event listeners for the 'dragstart' and 'dragend' events on each item
+    // in the list. When an item is dragged, it is set to 'none' so that it can be
+    // dragged, and when the drag ends, it is set back to 'block' so that it can be seen again.
     item.addEventListener('dragstart', function() {
         draggedItem = item;
         setTimeout(function () {
@@ -20,6 +24,13 @@ for (let i = 0; i < list_items.length; i++) {
         }, 0);
     });
 
+    //This code is setting up event listeners for a list of elements. The 'dragover' event
+    // listener prevents the default behavior when an item is dragged over the element.
+    // The 'dragenter' event listener sets the background color of the element to a semi-transparent
+    // black when an item is dragged over it. The 'dragleave' event listener sets the background
+    // color back to a semi-transparent black when an item is dragged away from it.
+    // Finally, the 'drop' event listener adds the dragged item to the list and sets the
+    // background color back to a semi-transparent black.
     for (let j = 0; j < lists.length; j++) {
         const list = lists[j];
 
@@ -42,3 +53,10 @@ for (let i = 0; i < list_items.length; i++) {
         })
     }
 }
+
+
+// This code is used to create a drag and drop feature. It selects all list items with 
+// the class 'list-item' and all lists with the class 'list'. It then adds event listeners 
+// to each list item to allow them to be dragged and dropped into the lists. When an item
+// is dragged, it is set as the 'draggedItem' variable and its display is set to 'none'.
+// When an item is dropped, it is appended to the list and its display is set back to 'block'.
